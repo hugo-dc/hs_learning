6cb99bdcc120a44a333866938ed4a8cd750a2acd
Jose Hugo De La Cruz Romero <hugo.delacruz@live.com.mx>
2015-02-20 10:00:26 -0600

    add type signatures

--- euler/p1.hs
+++ euler/p1.hs
@@ -4,7 +4,8 @@
 --
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
+result :: Int
 result = sum [x | x <- [1..999], x `mod` 3 == 0 ||  x `mod` 5 == 0]
 
-main = do 
-    putStr $ show result
+main :: IO ()
+main = putStr $ show result
